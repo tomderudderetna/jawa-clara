8a5a8a1beadc97d4ad21f6e958c2eed49ace9f21
Tom <tomderudder@gmail.com>
2017-09-13 20:30:44 +0200

    Nouvelle foncionalité: drag & drop word > formulaire Nouvelle foncionalité: donwload du sujet

--- app/creator.php
+++ app/creator.php
@@ -1,15 +1,11 @@
 <?php
 function get_include_contents($filename) {
-  if (is_file($filename)) {
-    ob_start();
-    include $filename;
-    $contents = ob_get_contents();
-    ob_end_clean();
-    return $contents;
-  }
-  return FALSE;
+  ob_start();
+  include $filename;
+  $contents = ob_get_contents();
+  ob_end_clean();
+  return $contents;
 }
-
 $data = get_include_contents('incluator.php');
 file_put_contents("sujet.html", $data);
 ?>
