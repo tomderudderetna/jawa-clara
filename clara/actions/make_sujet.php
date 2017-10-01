c57208d8448a1aa0d98d5de358ca93ab5598d190
Tom <tomderudder@gmail.com>
2017-10-01 22:28:58 +0200

    Clara - update architecture

---
+++ clara/actions/make_sujet.php
@@ -0,0 +1,11 @@
+<?php
+function get_include_contents($filename) {
+  ob_start();
+  include $filename;
+  $contents = ob_get_contents();
+  ob_end_clean();
+  return $contents;
+}
+$data = get_include_contents('..//views/render/index.php');
+file_put_contents("../sujet.html", $data);
+echo "ok";
